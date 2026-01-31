98851381cb3035ff8f3856d90777324b8e4322b7
petru-xval <filipe.petruccelli@xval.com.br>
2026-01-31 14:14:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 export default defineConfig({
-  base: "/xval.lp.web/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
